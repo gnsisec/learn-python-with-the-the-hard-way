e00332e1eee2d07e5488677406c05e2c845cc10d
Matius Kristian <Mattivityroom@gmail.com>
2017-02-22 20:28:14 +0700

    Exercise 1: A Good First Program -  Drill: Make your script print another line.

---
+++ ex1AnotherLine.py
@@ -0,0 +1,15 @@
+#This is Python version 2.7.12
+#Exercise 1: A Good First Program
+#Drill: Make your script print another line.
+
+print "Hello World!"
+print "Hello Again"
+print "I Like typing this. LOL"
+print "This is fun. LOL"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "\nI print in another line WOHOOOO!"
+
+#hint: use \n to enter the other zone of line
