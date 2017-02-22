2ea8d783bd7d1c71fad01a8ab3fae03e6cbab843
Matius Kristian <Mattivityroom@gmail.com>
2017-02-22 20:30:34 +0700

    Exercise 1: A Good First Program - Drill: Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.

---
+++ ex1PutOctothorpe.py
@@ -0,0 +1,13 @@
+#This is Python version 2.7.12
+#Exercise 1: A Good First Program
+#Drill: Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+# print "Hello World!"
+print "Hello Again"
+print "I Like typing this. LOL"
+print "This is fun. LOL"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#hint: use # to comment only one line
