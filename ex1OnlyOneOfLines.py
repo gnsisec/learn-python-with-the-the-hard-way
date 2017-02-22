ec04beb66aab1db1daf783c7c3fa9455e1abcecb
Matius Kristian <Mattivityroom@gmail.com>
2017-02-22 20:29:29 +0700

    Exercise 1: A Good First Program - Drill: Make your script print only one of the lines.

---
+++ ex1OnlyOneOfLines.py
@@ -0,0 +1,17 @@
+#This is Python version 2.7.12
+#Exercise 1: A Good First Program
+#Drill: Make your script print only one of the lines.
+
+'''
+print "Hello World!"
+print "Hello Again"
+print "I Like typing this. LOL"
+print "This is fun. LOL"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+'''
+
+print "I print only one WOHOOOO!"
+
+#hint: use ''' to comment more than 1 line
