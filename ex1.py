c0c4326caec5e021907cb656cc09f4fa4d9877b5
Matius Kristian <Mattivityroom@gmail.com>
2017-02-22 07:34:37 +0700

    Exercise 1: A Good First Program

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#This is Python version 2.7.12
+#Exercise 1: A Good First Program
+
+print "Hello World!"
+print "Hello Again"
+print "I Like typing this. LOL"
+print "This is fun. LOL"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
