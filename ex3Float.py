dabb759071dbb750a49ee0a6b47b982cfba3651a
Matius Kristian <Mattivityroom@gmail.com>
2017-02-25 14:46:03 +0700

    Exercise 3: Numbers and Math - Drill: Floating point

---
+++ ex3Float.py
@@ -0,0 +1,24 @@
+#This is Python version 2.7.12
+#Exercise 3: Numbers and Math
+#Drill: Floating point
+
+print "I will now count my cell:"
+print "Hens", 25.23 + 30.00 / 6
+print "Roosters", 100 - 25 * 3 % 4.00
+
+print "\nNow I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "\nIs it true that 3.02 + 2 < 7.01 - 2?"
+print 3.02 + 2 < 7.01 - 2
+
+print "\nWhat is 3.02 + 2?", 3.02 + 2
+print "What is 7.01 - 2?", 7.01 - 2
+
+print "\nOh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.02 > 5.01
+print "Is it greater or equal?", 5.02 >= 5.01
+print "Is it less or equal", 5.02 <= 5.01
