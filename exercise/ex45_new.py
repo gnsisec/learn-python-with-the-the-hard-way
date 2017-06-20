888e36bde66ebb3bb9acd8384f678228a3d67ac3
Matius Kristian <Mattivityroom@gmail.com>
2017-06-20 21:12:18 +0700

    Add some gather in #

--- exercise/ex45_new.py
+++ exercise/ex45_new.py
@@ -1,6 +1,6 @@
 class level():
     def level_one(self):
-        print "###      This is Level one       ###"
+        print "####      This is Level one       ####"
         self.math_one = input("Give me number 6 * 2: ")
         if self.math_one == 12:
             print "You are right!\n"
@@ -9,7 +9,7 @@ class level():
             exit(0)
 
     def level_two(self):
-        print "###      This is Level two       ###"
+        print "####      This is Level two       ####"
         self.math_two = input("Give me number 8 / 2: ")
         if self.math_two == 4:
             print "You are right!\n"
@@ -18,7 +18,7 @@ class level():
             exit(0)
 
     def level_three(self):
-        print "###      This is Level three     ###"
+        print "####      This is Level three     ####"
         self.math_three = input("Give me number 10 - 5: ")
         if self.math_three == 5:
             print "You are right!\n"
@@ -32,7 +32,9 @@ class game():
         self.mission = level()
 
     def play(self):
-        print "###      Welcome To XXX Game     ###\n\n"
+        print "######################################"
+        print "####     Welcome To XXX Game     #####"
+        print "######################################\n\n"
 
         self.mission.level_one()
         self.mission.level_two()
